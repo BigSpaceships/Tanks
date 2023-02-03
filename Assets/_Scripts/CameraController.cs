fc769e74cfa9e42c4cebe7e27ca46732923562c9
BigSpaceships <bigspaceships56@gmail.com>
2023-02-03 11:36:25 -0500

    FIX: Camera Clipping

--- Assets/_Scripts/CameraController.cs
+++ Assets/_Scripts/CameraController.cs
@@ -15,11 +15,17 @@ public class CameraController : MonoBehaviour {
     [SerializeField] private float pitch;
     [SerializeField] private float yaw;
 
+    private Camera _camera;
+
     private float _zoomInput;
     private Vector2 _orbitInput;
 
+    public bool[] display;
+
     private void Awake() {
         _zoomInput = 0;
+
+        _camera = transform.GetChild(0).GetComponent<Camera>();
     }
 
     private void OnEnable() {
@@ -34,20 +40,30 @@ public class CameraController : MonoBehaviour {
 
     private void Update() {
         distanceFromTank -= _zoomInput * zoomSpeed;
-        distanceFromTank = Mathf.Clamp(distanceFromTank, distanceRange.x, distanceRange.y);
 
-        pitch += _orbitInput.y * sensitivity;
+        pitch -= _orbitInput.y * sensitivity;
         yaw += _orbitInput.x * sensitivity;
 
-        pitch = Mathf.Clamp(pitch, 0, 85);
-
-        transform.GetChild(0).localPosition = new Vector3(0, 0, -distanceFromTank);
+        pitch = Mathf.Clamp(pitch, -20, 85);
 
         transform.rotation = Quaternion.Euler(pitch, 0, 0);
 
         transform.Rotate(Vector3.up, yaw, Space.World);
 
-        transform.position = tank.transform.position;
+        transform.position = tank.transform.Find("Camera Follow").position;
+
+        var layerMask = LayerMask.GetMask("Default");
+
+        display = MaskToBools(layerMask);
+
+        var maxDistance = Physics.SphereCast(transform.position, .5f, -transform.forward, out RaycastHit hit,
+            distanceRange.y, layerMask)
+            ? hit.distance
+            : distanceRange.y;
+
+        var actualDistanceFromTank = Mathf.Clamp(distanceFromTank, distanceRange.x, maxDistance);
+
+        _camera.transform.localPosition = new Vector3(0, 0, -actualDistanceFromTank);
     }
 
     private void OnZoom(InputAction.CallbackContext context) {
@@ -60,11 +76,18 @@ public class CameraController : MonoBehaviour {
     }
 
     private void OnOrbit(InputAction.CallbackContext context) {
-        if (context.canceled) {
-            _orbitInput = Vector2.zero;
+        _orbitInput = context.canceled ? Vector2.zero : context.ReadValue<Vector2>();
+    }
+
+    private bool[] MaskToBools(int layerMask) {
+        bool[] hasLayers = new bool[32];
+
+        for (int i = 0; i < 32; i++) {
+            if (layerMask == (layerMask | (1 << i))) {
+                hasLayers[i] = true;
+            }
         }
-        else {
-            _orbitInput = context.ReadValue<Vector2>();
-        }
+
+        return hasLayers;
     }
 }
