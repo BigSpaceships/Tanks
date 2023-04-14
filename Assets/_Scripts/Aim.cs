4045abf96ef6da6302d256a2054dd95fd1b8e1e9
BigSpaceships <bigspaceships56@gmail.com>
2023-04-14 11:48:18 -0400

    CHORE: started working on aiming target thing

---
+++ Assets/_Scripts/Aim.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class Aim : MonoBehaviour {
+    [SerializeField] private GameObject aimObject;
+
+    void Update() {
+        // var pos = Camera.main.ScreenPointToRay(new Vector3(Mouse.current.position.val, Mouse.current.position.y));
+    }
+}
